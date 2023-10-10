1c42d454302f24d01b1c8cad99276b6081b6ce16
LurA-AruL <arulramalingam06@gmail.com>
2023-10-10 15:23:04 +0530

    commit

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -114,8 +114,8 @@ export default function Cart({cartreciveFun}) {
         <div className='w-100 my-5'>
             <img src="assests/emptyCart_moblieView.png" className='w-75 m-auto front_corsur ' alt='no image found' />
             <p>Your Carts is empty!</p>
-            <p>Looks like you haven't added any courses to your cart yet.</p>
-            <button className='btn border px-4 rounded-pill bg-success text-white py-2 fs-6'>ADD COURSES TO CART</button>
+            <p>Looks like you haven't added any items to your cart yet.</p>
+            <button className='btn border px-4 rounded-pill bg-success text-white py-2 fs-6'>ADD items TO CART</button>
         </div>
       </div> :
 
